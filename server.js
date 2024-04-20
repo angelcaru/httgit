5dc61282a7c729c06536e7c310e815eaeabc8489
angelcaru <angelcaru10@gmail.com>
2024-04-20 12:27:11 +0200

    refactor: Factor out the git calling logic

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 
+const path = require("path");
+const chp = require("child_process");
+const express = require("express");
+
 const { argv } = process;
 const interp_name = argv.shift();
 const program_name = argv.shift();
@@ -6,11 +10,19 @@ if (argv.length < 1) {
     console.error(`Usage: ${interp_name} ${program_name} <repo>`);
     process.exit(1);
 }
-const repo = argv.shift();
+const repoDir = argv.shift();
+const repoDirAbs = path.resolve(repoDir);
+const repo = path.basename(repoDirAbs);
 
-const chp = require("child_process");
-const express = require("express");
 const app = express();
+
+function git(...args) {
+    return chp.spawnSync("git", args, {
+        cwd: repoDir,
+        encoding: "utf8",
+        stdio: "pipe",
+    }).stdout;
+}
 
 app.get("/", (req, res) => {
     res.sendFile(`${process.cwd()}/client/index.html`);
@@ -21,31 +33,19 @@ app.get("/index.js", (req, res) => {
 });
 
 app.get("/dyn.js", (req, res) => {
-    const gitBranchOut = chp.spawnSync("git", ["branch", "-a"], {
-        cwd: repo,
-        encoding: "utf8",
-        stdio: "pipe",
-    }).stdout;
+    const gitBranchOut = git("branch", "-a");
     const rawBranches = gitBranchOut.split("\n");
     const branches = rawBranches.map(raw => raw.slice(2)).filter(raw => raw.length > 0);
     const currentBranch = rawBranches.find(raw => raw.startsWith("*")).slice(2);
 
-    const gitLogOut = chp.spawnSync("git", ["log", "--pretty=format:%h %H %s"], {
-        cwd: repo,
-        encoding: "utf8",
-        stdio: "pipe",
-    }).stdout;
+    const gitLogOut = git("log", "--pretty=format:%h %H %s");
     const commits = gitLogOut.split("\n").map(raw => {
         const split = raw.split(" ");
         return {
             shortHash: split[0],
             hash: split[1],
             message: split.slice(2).join(" "),
-            body: chp.spawnSync("git", ["log", "-1", split[1], "--format=%B"], {
-                cwd: repo,
-                encoding: "utf8",
-                stdio: "pipe",
-            }).stdout,
+            body: git("log", "-1", split[1], "--format=%B"),
         };
     });
 
@@ -59,11 +59,7 @@ app.get("/dyn.js", (req, res) => {
 });
 
 app.get("/api/diff/:hash", (req, res) => {
-    const diff = chp.spawnSync("git", ["show", req.params.hash, "--format="], {
-        cwd: repo,
-        encoding: "utf8",
-        stdio: "pipe",
-    }).stdout;
+    const diff = git("show", req.params.hash, "--format=");
     res.send(diff);
 })
 
